5edcb824a84fdfb50db5c83b52deefd6def85d79
IN007868 <IN007868@10.172.32.250>
2019-09-26 03:12:00 -0700

    commit by me

---
+++ gitDemo/src/gitDemo/configGit.java
@@ -0,0 +1,10 @@
+package gitDemo;
+
+public class configGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("U have successfully done Github Configuration!!! ");
+	}
+
+}
